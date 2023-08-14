adf94db4bc2c2456b403ba1c4718aeffae01ffe9
Shreejal Dahal <shreejaldahal1@gmail.com>
2023-08-14 16:01:09 +0545

    registration completed

--- app/src/main/java/com/example/holiday/Login.java
+++ app/src/main/java/com/example/holiday/Login.java
@@ -5,8 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    TextView register;
+    Button login;
+    EditText username, password;
+    ProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +23,15 @@ public class Login extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+        username = findViewById(R.id.usernmae);
+        password = findViewById(R.id.password);
+        progress = findViewById(R.id.progress);
+
+
+
+
     }
 }
